256a4209dd8bb1141835ed1cf4521d32d1e78197
nikodem redmann <nikodem.redmann@wp.pl>
2022-11-30 13:53:31 +0100

    steampixel2

--- index.php
+++ index.php
@@ -1,19 +1,67 @@
 <?php
+
+use Steampixel\Route;
+
 require_once('config.php');
 require_once('class/User.class.php');
 
-$user = new User('jkowalski', 'tajneHasło');
-/*
-if($user->register()) {
-    echo "Zarejestrowano poprawnie";
-} else {
-    echo "Błąd rejestracji użytkownika";
-}
-*/
-if($user->login()) {
-    echo "Zalogowano poprawnie";
-} else {
-    echo "Błędny login lub hasło";
-}
+Route::add('/', function() {
+    echo "Strona główna";
+});
 
+Route::add('/login', function() { 
+    global $twig;
+    $twig->display('login.html.twig');
+});
+
+Route::add('/login', function() {
+    global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        if($user->login()) {
+            $v = array(
+                'message' => "Zalogowano poprawnie użytkownika: ".$user->getName(),
+            );
+            $twig->display('message.html.twig', $v);
+        } else {
+            $twig->display('login.html.twig', 
+                                ['message' => "Błędny login lub hasło"]);
+        }
+    } else {
+        die("Nie otrzymano danych");
+    }
+}, 'post');
+
+Route::add('/register', function() {
+    global $twig;
+    $twig->display('register.html.twig');
+});
+Route::add('/register', function() {
+    global $twig;
+    if(isset($_REQUEST['login']) && isset($_REQUEST['password'])) {
+        if(empty($_REQUEST['login']) || empty($_REQUEST['password'])
+            || empty($_REQUEST['firstName']) || empty($_REQUEST['lastName'])) {
+                //podano pusty string jako jedną z wymaganych wartości
+                $twig->display('register.html.twig', 
+                                ['message' => "Nie podano wymaganej wartości"]);
+                exit();
+            }
+        $user = new User($_REQUEST['login'], $_REQUEST['password']);
+        $user->setFirstName($_REQUEST['firstName']);
+        $user->setLastName($_REQUEST['lastName']);
+        if($user->register()) {
+            //echo "Zarejestrowano poprawnie";
+            $twig->display('message.html.twig', 
+                                ['message' => "Zarejestrowano poprawnie"]);
+        } else {
+            //echo "Błąd rejestracji użytkownika";
+            $twig->display('register.html.twig', 
+                                ['message' => "Błąd rejestracji użytkownika"]);
+        }
+    } else {
+        die("Nie otrzymano danych");
+    }
+}, 'post');
+
+Route::run('/loginform');
 ?>
